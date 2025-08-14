40fcde62b11cca517673023238be3631e8fe8689
Stephen McKeon <stephen.mckeon@powerhrg.com>
2025-08-14 09:40:26 -0400

    Add clickbait validations for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,24 @@ class Post < ApplicationRecord
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+
+  validate :clickbait
+
+private
+
+  def clickbait
+    return unless title
+    return if includes_clickbait_phrase(title) || includes_top_number(title)
+
+    errors.add(:title, "is not clickbaity enough")
+  end
+
+  def includes_clickbait_phrase(title)
+    phrases = ["Won't Believe", "Secret", "Guess"]
+    phrases.any? { |phrase| title.include?(phrase) }
+  end
+
+  def includes_top_number(title)
+    !!(title =~ /\bTop\s+\d+\b/)
+  end
 end
